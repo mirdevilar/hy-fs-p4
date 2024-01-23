54f64e83a9588515cc447635a92bded61df74e95
mirdevilar <dev@mirdevilar.com>
2024-01-23 23:18:51 +0100

    stable

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,8 +1,11 @@
-require('dotenv').config()
 const express = require('express')
-const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
+const config = require('./utils/config')
+const log = require('./utils/logger')
+
+const app = express()
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -22,7 +25,7 @@ app.use(express.json())
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
-    .then(blogs => {
+    .then((blogs) => {
       response.json(blogs)
     })
 })
@@ -32,12 +35,12 @@ app.post('/api/blogs', (request, response) => {
 
   blog
     .save()
-    .then(result => {
+    .then((result) => {
       response.status(201).json(result)
     })
 })
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  log.info(`Server running on port ${PORT}`)
 })
---
+++ bloglist/utils/logger.js
@@ -0,0 +1,9 @@
+const info = (...params) => {
+  console.log(...params)
+}
+
+const err = (...params) => {
+  console.error(...params)
+}
+
+module.exports = { info, err }
