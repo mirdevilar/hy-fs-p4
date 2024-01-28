4404fa23097e7ec8cab81b0782653e2b319898e3
mirdevilar <dev@mirdevilar.com>
2024-01-29 00:22:56 +0100

    4.5

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -4,8 +4,15 @@ const dummy = (blogs) => (1)
 
 const totalLikes = (blogs) => {
   const likes = blogs.reduce((sum, b) => sum + b.likes, 0)
-  log.info(likes)
   return likes
 }
 
-module.exports = { dummy, totalLikes }
+const mostLikes = (blogs) => {
+  const mostLikesIndex = blogs.reduce((maxLikesIndex, blog, i) => {
+    return blog.likes > blogs[maxLikesIndex].likes ? i : maxLikesIndex
+  }, 0)
+  //log.info(blogs[mostLikesIndex])
+  return blogs[mostLikesIndex]
+}
+
+module.exports = { dummy, totalLikes, mostLikes }
